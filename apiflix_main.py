f3aa509203d0b8e3ac8d09b5f4320fc5d19ea7ec
Carlos Miranda <ce.carv.miranda@gmail.com>
2020-05-28 21:35:16 -0300

    teste json loads

--- apiflix_main.py
+++ apiflix_main.py
@@ -106,7 +106,7 @@ def filter_by_country_page():
 def filter_api():
     result = import_files()
 
-    data = request.data.json.loads()
+    data = json.loads(request.data)
     data = data['actors']
     # result = result.to_dict(orient='index')
     result = result.iloc[0:100, :].to_json(orient='index')
